20642869d95e23f53d89310033866b43f7a5751f
Britta Daudert <bdaudert@dri.edu>
2014-12-08 15:30:17 -0800

    typo Lon --> Long fixed

--- templates/includes/scripts.php
+++ templates/includes/scripts.php
@@ -52,7 +52,7 @@
        		geocoder = new google.maps.Geocoder();
             var pointsLonLat = String({{pointsLonLat}});
             var pointLat = parseFloat(pointsLonLat.split(',')[1]);
-            var pointLon = parseFloat(pointsLonLat.split(',')[0]);
+            var pointLong = parseFloat(pointsLonLat.split(',')[0]);
 		var myCenter = new google.maps.LatLng({{ pointLat }}, {{ pointLong }});
 		var myZoom ={{ mapzoom }}
 		var mapOptions = {
