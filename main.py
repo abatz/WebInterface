4d48d17a3bbbe44291796b2daa4b1ba603435db7
Britta Daudert <bdaudert@dri.edu>
2014-12-07 18:26:10 -0800

    url_parameter read, dynamic colobar and get/post code cleanup

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import webapp2
 import datetime
 import numpy
 import json
-import httplib2 
+import httplib2
 
-from forms import *   
+from forms import *
 import collectionMethods
 from google.appengine.api import urlfetch
 urlfetch.set_default_fetch_deadline(60)
@@ -29,7 +29,7 @@ JINJA_ENVIRONMENT= jinja2.Environment(autoescape=True,
 ##       HOME PAGE                         ##
 #############################################
 class MainPage(webapp2.RequestHandler):
-    def get(self):   
+    def get(self):
         template = JINJA_ENVIRONMENT.get_template('home.html')
         self.response.out.write(template.render({}))
 
@@ -37,175 +37,115 @@ class MainPage(webapp2.RequestHandler):
 ##       DROUGHT TOOL PAGE                 ##
 #############################################
 class DroughtTool(webapp2.RequestHandler):
-#############################################
-##      GET                                ##
-#############################################
-    def get(self): 
-        ppost=0	
-        ee.Initialize(config.EE_CREDENTIALS, config.EE_URL)
+    def set_form_params(self):
+        #sets form  parameters
+        self.ppost = 0
+        self.opacity = self.request.get('opacity',str(14*0.05))
+        self.mapzoom = self.request.get('mapzoom',4)
+        self.variable = self.request.get('basicvariable','pr')
+        self.domainType = self.request.get('domainType','full')
+        self.state = self.request.get('state','California')
+        self.anomOrValue = self.request.get('anomOrValue','anom')
+        self.pointLatLong = self.request.get('pointLatLong','-112,42')
+        self.pointLatLongX = self.pointLatLong.split(",")
+        self.pointLong = float(self.pointLatLongX[0])
+        self.pointLat = float(self.pointLatLongX[1])
+        self.dateStart = self.request.get('dateStart','2013-01-01')
+        self.dateEnd = self.request.get('dateEnd','2013-03-31')
+        self.opacity = self.request.get('opacity',str(14*0.05))
+        self.NELat = self.request.get('NELat',45)
+        self.NELong= self.request.get('NELong',-95)
+        self.SWLat= self.request.get('SWLat',40)
+        self.SWLong= self.request.get('SWLong',-111)
+        self.minColorbar = self.request.get('minColorbar', None)
+        self.maxColorbar = self.request.get('maxColorbar', None)
+        self.palette = self.request.get('palette', None)
+        if self.minColorbar is None and self.maxColorbar is None:
+            self.palette,self.minColorbar,self.maxColorbar,self.colorbarLabel=collectionMethods.get_colorbar(self.variable,self.anomOrValue)
 
-	#mapzoom,pointLat,pointLong,state,variable,domainType,dateStart,dateEnd,anomOrValue,opacity,NELat,NELong,SWLat,SWLong\
-        #    =collectionMethods.initialize_form(self,ppost);
-        initial_mapzoom=4;
-        initial_pointLat=42;
-        initial_pointLong=-112;
-        initial_state='Calfornia';
-        initial_variable='pr';
-        initial_domainType='full';
-        initial_dateStart='2013-01-01';
-        initial_dateEnd='2013-03-31';
-        initial_anomOrValue='anom';
-        initial_opacity=str(14*0.05);
-        initial_NELat=45;
-        initial_NELong=-95;
-        initial_SWLat=40;
-        initial_SWLong=-111;
+    def set_share_link(self, initial_template_values):
+        shareLink = 'khegewisch-test.appspot.com?'
+        for key, val in initial_template_values.iteritems():
+            if str(key[0:4]) == 'form':
+                continue
+            if key == 'ppost':continue
+            #FIX ME: would be nice to not have to do this
+            #ie. change basicvariable form name to variable
+            if str(key) == 'variable':
+                param_str = 'basicvariable' + '=' + str(val)
+            else:
+                param_str = str(key) + '=' + str(val)
+            if shareLink[-1] =='?':
+                shareLink+=param_str
+            else:
+                shareLink+='&' + param_str
+        return shareLink
 
-        #initialize forms
-        mapzoom=self.request.get('mapzoom',initial_mapzoom);
-        pointLat=self.request.get('pointLat',initial_pointLat);
-        pointLong=self.request.get('pointLong',initial_pointLong);
-        state=self.request.get('state',initial_state);
-        variable=self.request.get('variable',initial_variable);
-        domainType=self.request.get('domainType',initial_domainType);
-        dateStart=self.request.get('dateStart',initial_dateStart);
-        dateEnd=self.request.get('dateEnd',initial_dateEnd);
-        anomOrValue=self.request.get('anomOrValue',initial_anomOrValue);
-        opacity=self.request.get('opacity',initial_opacity);
-        NELat=float(self.request.get('NELat',initial_NELat));
-        NELong=float(self.request.get('NELong',initial_NELong));
-        SWLat=float(self.request.get('SWLat',initial_SWLat));
-        SWLong=float(self.request.get('SWLong',initial_SWLong));
-
-        #if self.request.arguments():	
-        #    palette=self.request.get('palette','');
-        #    minColorbar=float(self.request.get('minColorbar',''));
-        #    maxColorbar=float(self.request.get('maxColorbar',''));
-	#    colorbarLabel='';
-        #    mapid,template_values,colorbarLabel,product,collectionLongName,notes,title,source,mapzoom= \
-        #        collectionMethods.get_images(opacity,pointLat,pointLong,NELat,NELong,SWLat,SWLong,ppost,\
-        #          variable,state,domainType,dateStart,dateEnd,anomOrValue,palette,minColorbar,maxColorbar);
-        #else:
-        palette,minColorbar,maxColorbar,colorbarLabel=collectionMethods.get_colorbar(variable,anomOrValue);
-
-        shareLink = 'khegewisch-test.appspot.com'+'?mapzoom='+str(mapzoom)+'?pointLat='+str(pointLat)+\
-            '?pointLong='+str(pointLong)+'?variable='+variable+'?opacity='+str(opacity)+'?dateStart='+dateStart+'?dateEnd='+dateEnd;
-             # not working currently... should only put variables in link that are different from the initialization values..
-             #if(mapzoom!=initial_mapzoom):
-             #  shareLink=shareLink+'?mapzoom='+str(mapzoom)
-             #if(pointLat!=initial_pointLat):
-             #  shareLink=shareLink+'?pointLat='+str(pointLat)
-             #if(pointLong!=initial_pointLong):
-             #shareLink=shareLink+'?pointLong='+str(pointLong)
-
-        base_template_values = {   #these are the same base as in POST
-            'shareLink': shareLink,
-            'opacity': opacity,
-            'pointLat': pointLat,
-            'pointLong': pointLong,
-            'NELat': NELat,
-            'NELong': NELong,
-            'SWLat': SWLat,
-            'SWLong': SWLong,
-            'ppost': ppost,
-            'mapzoom': mapzoom,
-            'variable': variable,
-            'state': state,
-            'domainType': domainType,
-            'dateStart': dateStart,
-            'dateEnd': dateEnd,
-            'anomOrValue': anomOrValue,
+    def set_initial_template_values(self):
+        template_values = {
+            'opacity': self.opacity,
+            'pointLat': self.pointLat,
+            'pointLong': self.pointLong,
+            'NELat': self.NELat,
+            'NELong': self.NELong,
+            'SWLat': self.SWLat,
+            'SWLong': self.SWLong,
+            'ppost': self.ppost,
+            'mapzoom': self.mapzoom,
+            'variable': self.variable,
+            'state': self.state,
+            'domainType': self.domainType,
+            'anomOrValue': self.anomOrValue,
+            'dateStart': self.dateStart,
+            'dateEnd': self.dateEnd,
+            'anomOrValue': self.anomOrValue,
             'formOpacity': formOpacity,
             'formAnomOrValue': formAnomOrValue,
             'formVariableGrid': formVariableGrid,
             'formLocation': formLocation,
             'formVariableLandsat': formVariableLandsat,
             'formStates': formStates,
-            'palette': palette,
-            'minColorbar': minColorbar,
-            'maxColorbar': maxColorbar,
-         }
-	template_values = base_template_values; 
+            #'palette': self.palette,
+            #'minColorbar': self.minColorbar,
+            #'maxColorbar': self.maxColorbar,
+        }
+        if self.palette:
+            template_values['palette']= self.palette
+        if self.minColorbar:
+            template_values['minColorbar']= self.minColorbar
+        if self.maxColorbar:
+            template_values['maxColorbar']= self.maxColorbar
+        template_values['shareLink'] = self.set_share_link(template_values)
+        if self.minColorbar:
+            template_values['minColorbar'] = self.minColorbar
+        if self.maxColorbar:
+            template_values['maxColorbar'] = self.maxColorbar
+        return template_values
+    #############################################
+    ##      GET                                ##
+    #############################################
+    def get(self):
+        ppost=0
+        ee.Initialize(config.EE_CREDENTIALS, config.EE_URL)
+
+        #initialize forms
+        self.set_form_params()
+        template_values = self.set_initial_template_values()
+        if self.request.arguments():
+            template_values = collectionMethods.get_images(template_values)
         template = JINJA_ENVIRONMENT.get_template('droughttool.php')
         self.response.out.write(template.render(template_values))
-
     #############################################
     ##      POST                                ##
     #############################################
     def post(self):
-        ppost=1
         ee.Initialize(config.EE_CREDENTIALS, config.EE_URL)
-
-        #Get data from form
-        opacity= self.request.get('opacity')
-        dateStart= self.request.get('dateStart')
-        dateEnd = self.request.get('dateEnd')
-        variable =self.request.get('basicvariable')
-        domainType =self.request.get('domainType')
-        state=self.request.get('state')
-        anomOrValue=self.request.get('anomOrValue')
-        pointLatLong = self.request.get('pointLatLong')
-        pointLatLongX = pointLatLong.split(",")
-        pointLong = float(pointLatLongX[0])
-        pointLat = float(pointLatLongX[1])
-        NELat = float(self.request.get('NELat'))
-        NELong = float(self.request.get('NELong'))
-        SWLat = float(self.request.get('SWLat'))
-        SWLong = float(self.request.get('SWLong'))
-
-        palette=self.request.get('palette');
-        minColorbar=float(self.request.get('minColorbar'));
-        maxColorbar=float(self.request.get('maxColorbar'));
-
-	if(variable=='wb'):
-            mapid,template_values,colorbarLabel,product,collectionLongName,notes,title,source,mapzoom,palette,minColorbar,maxColorbar=\
-	        collectionMethods.get_wb(opacity,pointLat,pointLong,NELat,NELong,SWLat,SWLong,ppost,\
-    	        state,domainType,dateStart,dateEnd,anomOrValue,palette,minColorbar,maxColorbar);
-	else:
-            mapid,template_values,colorbarLabel,product,collectionLongName,notes,title,source,mapzoom,palette,minColorbar,maxColorbar=\
-	        collectionMethods.get_images(opacity,pointLat,pointLong,NELat,NELong,SWLat,SWLong,ppost,\
-    	        variable,state,domainType,dateStart,dateEnd,anomOrValue,palette,minColorbar,maxColorbar);
-
-        shareLink = 'khegewisch-test.appspot.com'+'?mapzoom='+str(mapzoom)+'?pointLat='+str(pointLat)+\
-	'?pointLong='+str(pointLong)+'?variable='+variable+'?opacity='+str(opacity)+'?dateStart='+dateStart+'?dateEnd='+dateEnd;
-
-        extra_template_values = {
-            'shareLink': shareLink,
-            'colorbarLabel': colorbarLabel,
-            'opacity': opacity,
-            'pointLat': pointLat,
-            'pointLong': pointLong,
-            'NELat': NELat,
-            'NELong': NELong,
-            'SWLat': SWLat,
-            'SWLong': SWLong,
-            'product': product,
-            'productLongName': collectionLongName,
-            'notes': notes,
-            'variable': variable,
-            'state': state,
-            'domainType': domainType,
-            'dateStart': dateStart,
-            'dateEnd': dateEnd,
-            'anomOrValue': anomOrValue,
-            'ppost': ppost,
-            'title': title,
-            'source': source,
-            'mapzoom': mapzoom,
-            'mapid': mapid['mapid'],
-            'token': mapid['token'],
-            'formOpacity': formOpacity,
-            'formAnomOrValue': formAnomOrValue,
-            'formVariableGrid': formVariableGrid,
-            'formLocation': formLocation,
-            'formVariableLandsat': formVariableLandsat,
-            'formStates': formStates,
-            'palette': palette,
-            'minColorbar': minColorbar,
-            'maxColorbar': maxColorbar,
-        }
-        template_values = dict(template_values,**extra_template_values);
-
+        self.set_form_params()
+        template_values = self.set_initial_template_values()
+        #Override ppost default
+        template_values['ppost'] = 1
+        #get the collection and update template values
+        template_values = collectionMethods.get_images(template_values)
         template = JINJA_ENVIRONMENT.get_template('droughttool.php')
         self.response.out.write(template.render(template_values))
 
@@ -213,7 +153,7 @@ class DroughtTool(webapp2.RequestHandler):
 ##       CONTACT PAGE                      ##
 #############################################
 #class ContactPage(webapp2.RequestHandler):
-#	def get(self):   
+#	def get(self):
 #		template = JINJA_ENVIRONMENT.get_template('contact.html')
 #		self.response.out.write(template.render({}))
 
@@ -221,7 +161,7 @@ class DroughtTool(webapp2.RequestHandler):
 ##       ABOUT DATA PAGE                   ##
 #############################################
 #class DataPage(webapp2.RequestHandler):
-#	def get(self):   
+#	def get(self):
 #		template = JINJA_ENVIRONMENT.get_template('aboutdata.html')
 #		self.response.out.write(template.render({}))
 
@@ -229,7 +169,7 @@ class DroughtTool(webapp2.RequestHandler):
 ##       ABOUT METRIC PAGE                 ##
 #############################################
 #class MetricsPage(webapp2.RequestHandler):
-#	def get(self):   
+#	def get(self):
 #		template = JINJA_ENVIRONMENT.get_template('aboutmetrics.html')
 #		self.response.out.write(template.render({}))
 
