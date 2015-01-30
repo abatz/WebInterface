686821048da447112b9f621b0f137f785d018b19
khegewisch <khegewisch@uidaho.edu>
2015-01-30 08:52:28 -0800

    reduced size of loaded images and added javascript libraries rather than downloading from internet

--- templates/includes/basicscripts.php
+++ templates/includes/basicscripts.php
@@ -9,10 +9,14 @@
 <!------------------------------------>
 <!--  HIGH CHARTS                   -->
 <!------------------------------------>
-<script src="http://code.highcharts.com/highcharts.js"></script>
-<script src="http://code.highcharts.com/modules/exporting.js"></script>
+<!--<script src="http://code.highcharts.com/highcharts.js"></script>-->
+<script src="media/js/highcharts.js"></script>
+<!--<script src="http://code.highcharts.com/modules/exporting.js"></script>-->
+<script src="/media/js/exporting.js"></script>
 <!------------------------------------>
 <!--  D3 COLORBAR                   -->
 <!------------------------------------>
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-<script src="http://d3js.org/d3.v3.min.js"></script>
+
+<!--<script src="http://d3js.org/d3.v3.min.js"></script>-->
+<script src="/media/js/d3.v3.min.js"></script>
