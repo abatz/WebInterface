0821e3ae1b4ab6d38e305fdf12e2430afd343dda
Britta Daudert <bdaudert@dri.edu>
2015-02-04 18:22:08 -0800

    changed time series code to obtain data via getdownload url

--- figureFormatting.py
+++ figureFormatting.py
@@ -7,6 +7,82 @@ import collectionMethods
 #===========================================
 #  FORMAT_DATA_FOR_HIGHCHARTS
 #===========================================
+def set_time_series_data(dataList,dataList2,template_values):
+    '''
+    dataList -- main collection
+    dataList2 -- extra collections for derived variables tmean, wb
+    '''
+    var = template_values['variable']
+    units = template_values['units']
+    marker_colors = template_values['marker_colors']
+    timeSeriesData = [];timeSeriesGraphData = []
+    dates = list(zip(*dataList)[0])
+    lons = list(zip(*dataList)[1])
+    lats = list(zip(*dataList)[2])
+    times = list(zip(*dataList)[3])
+    data = list(zip(*dataList)[4])
+    #Id dataList2, we are dealing with wb or tmean
+    #and need to combine the data
+    #We also need to format the data slightly so that we don't have
+    #character strings in there
+    ts_data = []
+    graph_data =[]
+    for idx,d in enumerate(data):
+        date_string = dates[idx]
+        if(date_string[0:7]=='MCD43A4'):
+            date_string = date_string[12:16] + '-' + date_string[17:19] + '-' + date_string[20:22]
+        else:
+            i=date_string.rfind('_');
+            if(i==-1):
+                date_string = date_string[0:4] + '-' + date_string[4:6] + '-' + date_string[6:8]
+            else:
+                date_string = date_string[i+1:i+5] + '-' + date_string[i+5:i+7] + '-' + date_string[i+7:i+9]
+        try:
+            val = round(float(data[idx]),4)
+        except:
+            val = None
+        ts_data.append([date_string,val])
+        try:
+            graph_data.append([int(times[idx]),val])
+        except:
+            #vars wb, tmean do not return times.Need to convert to integer time
+            time_tuple = (int(date_string[0:4]),int(date_string[5:7]),int(date_string[8:10]),13, 59, 27, 2, 317, 0)
+            timestamp = int(time.mktime(time_tuple))
+            graph_data.append([timestamp,val])
+
+    '''
+    Note ee spits out data for points in one list,
+    stringing the point data together
+    '''
+    #Find unique lats, i.e unique points points
+    unique_lats = list(set(lats))
+    #Find the indices in the data where points change
+    point_indices = []
+    for lat in unique_lats:
+        point_indices.append(lats.index(lat))
+    point_indices = sorted(point_indices)
+    #Loop over points and compile data and graph data
+    timeSeriesData = []
+    timeSeriesGraphData = []
+    for i,p_idx in enumerate(point_indices):
+        i_start = p_idx
+        if i == len(point_indices) -1:
+            i_end = len(data)
+        else:
+            i_end = point_indices[i+1]
+        data_dict = {
+            'LongLat': str(round(lons[p_idx],4)) + ',' + str(round(lats[p_idx],4)),
+            'Data':ts_data[i_start:i_end]
+        }
+        data_dict_graph = {
+            'MarkerColor':marker_colors[i],
+            'LongLat': str(round(lons[p_idx],4)) + ',' + str(round(lats[p_idx],4)),
+            'Data':graph_data[i_start:i_end]
+        }
+        timeSeriesData.append(data_dict)
+        timeSeriesGraphData.append(data_dict_graph)
+    return timeSeriesData, timeSeriesGraphData
+
 def set_initial_time_series_data(dataList,dataList2,template_values):
     '''
     dataList -- main collection
