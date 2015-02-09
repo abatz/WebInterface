2c4cc49848da465d83421dbb7bf00f5238a07326
charles morton <charles.morton@dri.edu>
2015-02-09 11:15:54 -0800

    Missing PDSI in GRIDMET is causing error
    How should the code handling failing gracefully when the
    product/variable/band doesn't exist?

--- collectionMethods.py
+++ collectionMethods.py
@@ -183,11 +183,16 @@ def get_collection(product, variable):
 
     Args:
         product: single character indicating the product (G, M, 8, or 5)
-        variable: string indicating the variable within the product
+        variable: string indicating the variable/band to return
             (i.e. NDVI, EVI, pet, tmmn)
     Returns:
-        EarthEngine image collection
-
+        Output from product collection functions
+        Tuple of the following:
+            EarthEngine image collection object
+            String of the collection name
+            String of the collection description
+            String of the variable description
+            String of additional notes about the collection
     """
     if product == 'G':        
         return get_gridmet_collection(variable)
@@ -200,15 +205,28 @@ def get_collection(product, variable):
         ##return get_landsat457_daily_collection(variable)
     ##elif product == 'L':
     ##    return get_landsat_daily_collection(variable)
+    ## How should this function fail gracefully if the inputs are bad?
+    ## Should it return an exception?
+    ##else:
+    ##    pass
 
 def get_landsat457_daily_collection(variable):
-    """"""
+    """Return the daily merged image collection for Landsat 4, 5, and 7
+
+    Args:
+        variable: string indicating the variable/band to return
+            (NDVI, NDSI, NDWI, or EVI)
+    Returns:
+        EarthEngine image collection object
+        String of the collection name
+        String of the collection description
+        String of the variable description (may be the input variable string)
+        String of additional notes about the collection
+    """
     ## This string could/should be built based on the date range or looking at
     ##   or looking atthe images in the collection
     coll_name = 'LT4_L1T_TOA,LT5_L1T_TOA,LE7_L1T_TOA'
     coll_desc = 'Landsat 4/5/7 Daily {0} (cloud mask applied)'.format(variable)
-    ##coll_name = 'LT5_L1T_TOA'
-    ##coll_desc = 'Landsat 5, daily {0} (cloud mask applied)'.format(variable)
 
     ## Select variable after calculating index
     collection = ee.ImageCollection([])
@@ -229,11 +247,26 @@ def get_landsat457_daily_collection(variable):
     elif variable == 'EVI':
         notes = "EVI calculated from Near-IR, Red and Blue bands"
         collection = collection.map(landsat457_evi_func)
+    ## How should this function fail gracefully if the inputs are bad?
+    ## Should it return an exception?
+    else:
+        notes = ''
     collection = collection.select(variable)
     return collection, coll_name, coll_desc, variable, notes
 
 def get_landsat5_daily_collection(variable):
-    """"""
+    """Return the daily image collection for only Landsat 5
+
+    Args:
+        variable: string indicating the variable/band to return
+            (NDVI, NDSI, NDWI, or EVI)
+    Returns:
+        EarthEngine image collection object
+        String of the collection name
+        String of the collection description
+        String of the variable description (may be the input variable string)
+        String of additional notes about the collection
+    """
     coll_name = 'LT5_L1T_TOA'
     coll_desc = 'Landsat 5, daily {0} (cloud mask applied)'.format(variable)
     ## Select variable after calculating index
@@ -250,11 +283,26 @@ def get_landsat5_daily_collection(variable):
     elif variable == 'EVI':
         notes = "EVI calculated from Near-IR, Red and Blue bands"
         collection = collection.map(landsat457_evi_func)
+    ## How should this function fail gracefully if the inputs are bad?
+    ## Should it return an exception?
+    else:
+        notes = ''
     collection = collection.select(variable)
     return collection, coll_name, coll_desc, variable, notes
 
 def get_landsat8_daily_collection(variable):
-    """"""
+    """Return the daily image collection for only Landsat 8
+
+    Args:
+        variable: string indicating the variable/band to return
+            (NDVI, NDSI, NDWI, or EVI)
+    Returns:
+        EarthEngine image collection object
+        String of the collection name
+        String of the collection description
+        String of the variable description (may be the input variable string)
+        String of additional notes about the collection
+    """
     coll_name = 'LC8_L1T_TOA'
     coll_desc = 'Landsat 8, daily {0} (cloud mask applied)'.format(variable)
     ## Select variable after calculating index
@@ -273,12 +321,27 @@ def get_landsat8_daily_collection(variable):
     elif variable == 'EVI':
         notes = "EVI calculated from Near-IR, Red and Blue bands"
         collection = collection.map(landsat8_evi_func)
+    ## How should this function fail gracefully if the inputs are bad?
+    ## Should it return an exception?
+    else:
+        notes = ''
     collection = collection.select(variable)
     return collection, coll_name, coll_desc, variable, notes
 
 ## Landsat 8 Day collection functions
 ##def get_landsat457_8day_collection(variable):
-##    """"""
+##    """Return the merged 8 day composite image coll. for Landsats 4, 5, and 7
+##
+##    Args:
+##        variable: string indicating the variable/band to return
+##            (NDVI, NDSI, NDWI, or EVI)
+##    Returns:
+##        EarthEngine image collection
+##        String of the collection name object
+##        String of the collection description
+##        String of the variable description (may be the input variable string)
+##        String of additional notes about the collection
+##    """
 ##    coll_name = 'LT4_L1T_8DAY_{0},LT5_L1T_8DAY_{0},LE7_L1T_8DAY_{0}'.format(variable)
 ##    coll_desc = 'Landsat 4/5/7 8-day {0} Composite'.format(variable)
 ##    collection4 = ee.ImageCollection('LT4_L1T_8DAY_{0}'.format(variable))
@@ -294,10 +357,25 @@ def get_landsat8_daily_collection(variable):
 ##        notes = "NDWI calculated from Norm. Diff. of near-IR and mid-IR bands"
 ##    elif variable == 'EVI':
 ##        notes = "EVI calculated from Near-IR, Red and Blue bands"
+##    ## How should this function fail gracefully if the inputs are bad?
+##    ## Should it return an exception?
+##    else:
+##        notes = ''
 ##    return collection, coll_name, coll_desc, product, variable, notes
 ##
 ##def get_landsat8_8day_collection(variable):
-##    """"""
+##    """Return the 8 day composite image collection for only Landsat 8
+##
+##    Args:
+##        variable: string indicating the variable/band to return
+##            (NDVI, NDSI, NDWI, or EVI)
+##    Returns:
+##        EarthEngine image collection object
+##        String of the collection name
+##        String of the collection description
+##        String of the variable description (may be the input variable string)
+##        String of additional notes about the collection
+##    """
 ##    coll_name = 'LT5_L1T_8DAY_{0}'.format(variable)
 ##    coll_desc = 'Landsat 5, 8-day {0} Composite'.format(variable)
 ##    collection = ee.ImageCollection('LT5_L1T_8DAY_{0}'.format(variable)).select(variable)
@@ -309,10 +387,25 @@ def get_landsat8_daily_collection(variable):
 ##        notes = "NDWI calculated from Norm. Diff. of near-IR and mid-IR bands"
 ##    elif variable == 'EVI':
 ##        notes = "EVI calculated from Near-IR,Red and Blue bands"
+##    ## How should this function fail gracefully if the inputs are bad?
+##    ## Should it return an exception?
+##    else:
+##        notes = ''
 ##    return collection, coll_name, coll_desc, variable, notes
     
 def get_modis_16day_collection(variable):
-    """ """
+    """Return the 16 day composite image collection for MODIS
+
+    Args:
+        variable: string indicating the variable/band to return
+            (NDVI, NDSI, NDWI, or EVI)
+    Returns:
+        EarthEngine image collection object
+        String of the collection name
+        String of the collection description
+        String of the variable description (may be the input variable string)
+        String of additional notes about the collection
+    """
     coll_name = 'MCD43A4_{0}'.format(variable)
     coll_desc = 'MODIS 16-day {0}'.format(variable)
     collection = ee.ImageCollection(coll_name).select(variable)
@@ -324,15 +417,29 @@ def get_modis_16day_collection(variable):
         notes = "NDWI calculated from Norm. Diff. of near-IR and mid-IR bands"
     elif variable == 'EVI':
         notes = "EVI calculated from Near-IR,Red and Blue bands"
+    ## How should this function fail gracefully if the inputs are bad?
+    ## Should it return an exception?
+    else:
+        notes = ''
     return collection, coll_name, coll_desc, variable, notes
     
 def get_gridmet_collection(variable):
-    """ """
+    """Return the daily image collection for GRIDMET
+
+    Args:
+        variable: string indicating the variable/band to return
+            (i.e. pr, tmmx, rmin, vs, pet, etc.)
+    Returns:
+        EarthEngine image collection object
+        String of the collection name
+        String of the collection description
+        String of the variable description (may be the input variable string)
+        String of additional notes about the collection
+    """
     coll_name = 'IDAHO_EPSCOR/GRIDMET'
     coll_desc = 'gridMET 4-km observational dataset(University of Idaho)'
     # Don't select variable here since Tmean or WB need to be mapped/calculated first
-    collection = ee.ImageCollection(coll_name).select(variable)
-    notes = ""
+    collection = ee.ImageCollection(coll_name)
     if variable == 'pr':
         var_desc = 'Precipitation'
     elif variable == 'tmmx':
@@ -355,7 +462,7 @@ def get_gridmet_collection(variable):
         notes = "ASCE Standardized Reference ET, estimated using the Penmann Monteith method. See Equation 1 in http://www.kimberly.uidaho.edu/water/asceewri/ascestzdetmain2005.pdf"
         var_desc = 'Reference Evapotranspiration'
     elif variable == 'pdsi':
-        ##notes = ""
+        notes = ""
         var_desc = 'Palmer Drought Severity Index (PDSI)'
     elif variable == 'tmean':
         collection = collection.map(gridmet_tmean_func)
@@ -365,6 +472,11 @@ def get_gridmet_collection(variable):
         collection = collection.map(gridmet_wb_func)
         notes = "Calculated as the difference between precipitation and reference evapotranspiration"
         var_desc = 'Water Balance (PPT-PET)'
+    ## How should this function fail gracefully if the inputs are bad?
+    ## Should it return an exception?
+    else:
+        notes = ""
+        var_desc = ""
     collection = collection.select(variable)
     return collection, coll_name, coll_desc, var_desc, notes
 
@@ -399,6 +511,10 @@ def landsat457_evi_func(img):
         .select([0], ['EVI'])\
         .copyProperties(img, ['system:index', 'system:time_start'])
 
+## DEADBEEF - Need to code in Landsat 8 cloud masking
+def landsat8_cloud_mask_func(img):
+    return img
+
 def landsat8_ndvi_func(img):
     """Calculate NDVI for a daily Landsat 8 image"""
     ## Remove .clamp(-0.1, 1)
@@ -428,15 +544,14 @@ def gridmet_wb_func(img):
     """Calculate water balance from precip and PET for GRIDMET collection"""
     pr_img = img.select('pr')
     img = pr_img.subtract(img.select('pet')).select([0],['wb'])
-    return ee.Image(img.copyProperties(
-        img_pr, ['system:index', 'system:time_start']))
+    return img.copyProperties(img, ['system:index', 'system:time_start'])
 
 def gridmet_tmean_func(img):
     """Calculate Tmean image from Tmin and Tmax for GRIDMET collection"""
-    img_tmmx = img.select('tmmx')
-    img = img_tmmx.add(img.select('tmmn')).multiply(0.5).select([0],['tmean'])
-    return ee.Image(img.copyProperties(
-        img_tmmx, ['system:index', 'system:time_start']))
+    tmax_img = img.select('tmmx')
+    tmin_img = img.select('tmmx')
+    tmean_img = tmax_img.add(tmin_img).multiply(0.5).select([0],['tmean'])
+    return tmean_img.copyProperties(img, ['system:index', 'system:time_start'])
 
 #===========================================
 #    GET_ANOMALY
