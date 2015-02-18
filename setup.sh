a594dc3332f494ca12b2b75787232376d5964b6d
khegewisch <khegewisch@uidaho.edu>
2015-02-18 11:26:13 -0800

    updated ee version and fixed EVI

--- setup.sh
+++ setup.sh
@@ -13,7 +13,9 @@ sed "s/<your-appid-here>/$APPENGINE_APP_ID/g" app.yaml.bak > app.yaml
 ln -s $HOME/.keys/privatekey.pem privatekey.pem
 mv config.py config.py.bak
 sed "s/EE_ACCOUNT = 'your-service-account-id@developer.gserviceaccount.com'/EE_ACCOUNT = '$MY_SERVICE_ACCOUNT'/g" config.py.bak > config.py
-ln -s $HOME/anaconda/envs/ee-python/lib/python2.7/site-packages/ee ee
+#ln -s $HOME/anaconda/envs/ee-python/lib/python2.7/site-packages/ee ee
+pip install earthengine-api --upgrade 
+ln -s $HOME/anaconda/lib/python2.7/site-packages/ee
 ln -s $HOME/anaconda/envs/ee-python/lib/python2.7/site-packages/oauth2client/ oauth2client
 ln -s $HOME/anaconda/envs/ee-python/lib/python2.7/site-packages/six.py six.py
 ln -s $HOME/anaconda/envs/ee-python/lib/python2.7/site-packages/httplib2 httplib2
